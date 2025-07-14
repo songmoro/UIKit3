637974f679b35e32125eb9ead8716d6804756829
songmoro <wlqgkr@naver.com>
2025-07-14 10:22:42 +0900

    TravleTableViewCell init

---
+++ UIKit3/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  UIKit3
+//
+//  Created by 송재훈 on 7/14/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
