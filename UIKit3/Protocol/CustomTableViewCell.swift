bb376d3b1dbf5a541fc75d1d6f2ec358131e7307
songmoro <wlqgkr@naver.com>
2025-07-14 12:53:51 +0900

    replace CustomTableViewCell parameter -> placeholder

--- UIKit3/Protocol/CustomTableViewCell.swift
+++ UIKit3/Protocol/CustomTableViewCell.swift
@@ -18,7 +18,7 @@ protocol HasIdentifier {
 }
 
 extension UITableView {
-    func register(_ customCell: CustomTableViewCell.Type, bundle: Bundle? = nil) {
+    func register<T: CustomTableViewCell>(_ customCell: T.Type, bundle: Bundle? = nil) {
         self.register(
             UINib(nibName: customCell.nibName, bundle: bundle),
             forCellReuseIdentifier: customCell.identifier
