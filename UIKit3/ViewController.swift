8ae8d8a5dd04678b1f8572d3ddf3e133fb7d512c
songmoro <wlqgkr@naver.com>
2025-07-14 10:13:42 +0900

    add viewDidAppear toast

--- UIKit3/ViewController.swift
+++ UIKit3/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import Toast
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.makeToast("\(#file): viewDidAppear")
     }
 }
